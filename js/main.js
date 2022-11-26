e9dad64522ac11e8471783d4131295c4897e4b8b
devizee <dev.bedeisrael@gmail.com>
2022-11-26 19:33:18 +0100

    partners + about-maj + termes + politiques

--- js/main.js
+++ js/main.js
@@ -105,6 +105,34 @@
             }
         }
     });
+
+    // Vendor carousel
+    $('.vendor-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        dots: true,
+        loop: true,
+        center: true,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive: {
+            0:{
+                items:2
+            },
+            576:{
+                items:3
+            },
+            768:{
+                items:4
+            },
+            992:{
+                items:5
+            },
+            1200:{
+                items:6
+            }
+        }
+    });
     
 })(jQuery);
 
